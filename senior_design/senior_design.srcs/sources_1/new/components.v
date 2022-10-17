8395d5e7052b7f22afdf8cad915df24fa963c828
kylnan <kyl3zyl3@gmail.com>
2022-10-17 13:33:16 -0700

    Implmented AMS, shutdown, inertia, and clk

--- senior_design/senior_design.srcs/sources_1/new/components.v
+++ senior_design/senior_design.srcs/sources_1/new/components.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 module BSPD(
+    input clk,
     input [7:0]b1,
     input [7:0]a1,
     input [7:0]a2,
@@ -9,45 +10,73 @@ module BSPD(
 endmodule
 
 module IMD(
+    input clk,
     input [7:0]in,
-    output fault
+    output reg fault
     );
 endmodule
 
 module inertia(
+    input clk,
     input[7:0]in,
-    output fault
+    output reg fault
     );
+always @ (*)
+ begin
+    if (in <= 8'd11500)
+    begin
+        fault = 1'b1;
+    end
+ end
 endmodule
 
 module shutdown(
+    input clk,
     input left,
     input right,
     input center,
-    input fault
+    input fault,
+    output reg shutdown_signal
     );
+always @ (*)
+ begin
+    if (left || right || center || fault)
+        shutdown_signal = 1'b1;
+ end
 endmodule
 
 module AMS(
+    input clk,
     input [7:0]in,
-    output fault
+    output reg fault
     );
+
+always @ (*)
+ begin
+    if (in <= 8'd11500)
+    begin
+        fault = 1;
+    end
+ end
 endmodule
 
 module masterSwitch(
+    input clk,
     input [7:0]in,
-    output fault
+    output reg fault
     );
 endmodule
 
 module HVDInterlock(
+    input clk,
     input [7:0]in,
-    output fault
+    output reg fault
     );
 endmodule
 
 module missInterlock(
+    input clk,
     input [7:0]in,
-    output fault
+    output reg fault
     );
 endmodule
