08ed6b3af7d07116d8c989e70c52edd2ed64d2ba
KyleLeongZ <55963087+KyleLeongZ@users.noreply.github.com>
2022-11-15 15:10:43 -0800

    added power calculation and voltage and current inputs to bspd
    Signed-off-by: KyleLeongZ <55963087+KyleLeongZ@users.noreply.github.com>

--- senior_design/senior_design.srcs/sources_1/new/components.v
+++ senior_design/senior_design.srcs/sources_1/new/components.v
@@ -6,22 +6,34 @@ module BSPD(
     input [7:0]b1,
     input [7:0]a1,
     input [7:0]a2,
+    input signed [4:0]v,
+    input [3:0]c,
     output reg fault
     );
     
-    //counter reg
-    reg [23:0]counter;
+    reg [23:0]counter; //counter reg
+    reg [4:0]power; //power reg
+    reg [4:0]abs_v; //absolute value of voltage reg
     
     always@(posedge clk)begin
-        //checks if fault is high
-        if((b1 >= 8'd11500) && (a1 >= 8'd11500) || (a2 >= 8'd11500))
+        //gets abs value of the voltage
+        if(v[4] == 1'b1)
+            abs_v = -v;
+        else
+            abs_v = v;
+              
+        //calc power draw of motor
+        power = abs_v * c;
+        
+        //checks if fault is high or too much power is being drawn
+        if((b1 >= 8'd11500) && (a1 >= 8'd11500) || (a2 >= 8'd11500) || power >= 5'd5000)
             counter = counter + 1;
         else
             counter = 24'd0;
             
         //checks if it has been 0.5s
         if(counter >= 24'd12500000) begin
-            fault = 1;
+            fault = 1'b1;
         end else begin
             fault = 1'b0;
         end    
