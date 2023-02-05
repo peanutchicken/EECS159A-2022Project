061b5983c785ccc236ee16547d2728973c3c65a1
KyleLeongZ <55963087+KyleLeongZ@users.noreply.github.com>
2023-02-05 11:44:44 -0800

    Created Block Design

--- senior_design/senior_design.runs/synth_1/runme.sh
+++ senior_design/senior_design.runs/synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=C:/Xilinx/Vivado/2022.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2022.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2022.1/bin
+  PATH=D:/Xilinx/Vivado/2022.1/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2022.1/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2022.1/bin
 else
-  PATH=C:/Xilinx/Vivado/2022.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2022.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2022.1/bin:$PATH
+  PATH=D:/Xilinx/Vivado/2022.1/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2022.1/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2022.1/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.runs/synth_1'
+HD_PWD='D:/UCI/Senior Design/senior_design/senior_design.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- senior_design/senior_design.runs/synth_1/safety_circuit.tcl
+++ senior_design/senior_design.runs/synth_1/safety_circuit.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.runs/synth_1/safety_circuit.tcl"
+  variable script "D:/UCI/Senior Design/senior_design/senior_design.runs/synth_1/safety_circuit.tcl"
   variable category "vivado_synth"
 }
 
@@ -76,17 +76,17 @@ create_project -in_memory -part xc7k70tfbv676-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.cache/wt [current_project]
-set_property parent.project_path C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.xpr [current_project]
+set_property webtalk.parent_dir {D:/UCI/Senior Design/senior_design/senior_design.cache/wt} [current_project]
+set_property parent.project_path {D:/UCI/Senior Design/senior_design/senior_design.xpr} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.cache/ip [current_project]
+set_property ip_output_repo {d:/UCI/Senior Design/senior_design/senior_design.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_verilog -library xil_defaultlib {
-  C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.srcs/sources_1/new/components.v
-  C:/Users/Kyl3Z/OneDrive/Documents/GitHub/EECS159A-2022Project/senior_design/senior_design.srcs/sources_1/new/safety_circuit.v
+  {D:/UCI/Senior Design/senior_design/senior_design.srcs/sources_1/new/components.v}
+  {D:/UCI/Senior Design/senior_design/senior_design.srcs/sources_1/new/safety_circuit.v}
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
